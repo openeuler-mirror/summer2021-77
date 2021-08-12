bb7dc787bf3b0f719989a9266aba852bb065a907
root <root@localhost.localdomain>
2021-08-12 21:35:02 +0800

    checkpoint init

---
+++ src/cmd/isula/base/checkpoint.h
@@ -0,0 +1,13 @@
+//
+// Created by root on 2021/8/12.
+//
+
+#ifndef SUMMER2021_77_CHECKPOINT_H
+#define SUMMER2021_77_CHECKPOINT_H
+
+#endif //SUMMER2021_77_CHECKPOINT_H
+
+
+
+int cmd_checkpoint_main(int argc, const char **argv);
+int checkpoint(struct client_arguments *args);
