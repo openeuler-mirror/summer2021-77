8d8bc1646b1d2ce020b8fb77d4060a9a903cfd97
xiehengjian <846188037@qq.com>
2021-09-30 02:22:51 +0800

    fix:

--- checkpoint-test.sh
+++ checkpoint-test.sh
@@ -1,3 +1,3 @@
 /bin/sh -c 'i=0; while true; do echo $i >a.txt; i=$(expr $i + 1); sleep 1; done'
 
-isula run -d  --external-rootfs=/var/lib/isulad/storage/overlay/8f181e47046ea672f0432ff987b15e64c4a399986e5b04e5feea1b313ca68943/merged --security-opt seccomp=unconfined busybox /bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'
+isula run -d  --external-rootfs=/var/lib/isulad/storage/overlay/507395028f51f1c49facc13e19ac9e44d37d9ba2e2c5aab5ca1c8dd4b60065a0/merged --security-opt seccomp=unconfined ubuntu /bin/sh -c 'i=0; while true; do echo $i > a.txt; i=$(expr $i + 1); sleep 1; done'
