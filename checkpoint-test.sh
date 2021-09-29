7f2112e030bc2b236d383fe7285726500b9da08f
予留 <hengjianxie.xhj@alibaba-inc.com>
2021-09-30 01:59:50 +0800

    fix

--- checkpoint-test.sh
+++ checkpoint-test.sh
@@ -1,1 +1,3 @@
 /bin/sh -c 'i=0; while true; do echo $i >a.txt; i=$(expr $i + 1); sleep 1; done'
+
+isula run -d  --external-rootfs=/var/lib/isulad/storage/overlay/8f181e47046ea672f0432ff987b15e64c4a399986e5b04e5feea1b313ca68943/merged --security-opt seccomp=unconfined busybox /bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'
